5aa352965294ae412a79f107f0e932f4d1ccb83b
Abdumalik995 <abdumalik.botirov.95@mail.ru>
2019-04-29 10:13:34 +0300

    edu sayt faylga ulanish

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php 
-	
+
+	//1. Umumiy sozlash
+	ini_set('display_errors', 1);
+	error_reporting(E_ALL);
+	//2.faylga ulanish
+	define('ROOT', dirname(_FILE_));
+	require_once(ROOT.'components/Router.php');
  ?>
